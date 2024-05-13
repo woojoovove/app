caeb6fb66a327cbd2140882b31ec682fe10657ff
ujin_jeong <ujin_jeong@tmax.co.kr>
2024-05-13 13:28:16 +0900

    [add] user, group 엔티티

---
+++ src/main/java/com/example/app/data/entity/GroupsEntity.java
@@ -0,0 +1,19 @@
+package com.example.app.data.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "groups")
+public class GroupsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/example/app/data/entity/UsersEntity.java
@@ -0,0 +1,24 @@
+package com.example.app.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UsersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nickname;
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private GroupsEntity group;
+}
