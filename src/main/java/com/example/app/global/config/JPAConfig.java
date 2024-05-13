39d073af4a7b9ae3410fcc743b305d9ec114367a
ujin_jeong <ujin_jeong@tmax.co.kr>
2024-05-13 23:25:23 +0900

    [config] annotaionProcessor 변경
    javax > jakarta로 변경

--- src/main/java/com/example/app/global/config/JPAConfig.java
+++ src/main/java/com/example/app/global/config/JPAConfig.java
@@ -1,7 +1,7 @@
 package com.example.app.global.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
