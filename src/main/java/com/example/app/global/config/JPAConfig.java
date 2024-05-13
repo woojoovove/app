6993779fee676f0df7c3fc10eadd20f0540bcd0b
ujin_jeong <ujin_jeong@tmax.co.kr>
2024-05-13 22:46:14 +0900

    [mod] jpaConfig 파일 경로 변경

--- src/main/java/com/example/app/global/config/JPAConfig.java
+++ src/main/java/com/example/app/global/config/JPAConfig.java
@@ -1,7 +1,7 @@
-package com.example.app.data;
+package com.example.app.global.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
