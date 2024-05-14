6d77076df34384534249f445f9c36f511e2392a4
ujin_jeong <ujin_jeong@tmax.co.kr>
2024-05-15 07:42:34 +0900

    [fix] 엔티티 설계 변경 - 관계형 테이블 추가

--- src/main/java/com/example/app/data/entity/UsersEntity.java
+++ src/main/java/com/example/app/data/entity/UsersEntity.java
@@ -1,13 +1,12 @@
 package com.example.app.data.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -20,7 +19,6 @@ public class UsersEntity {
     private String nickname;
     private String token;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="group_id")
-    private GroupsEntity group;
+    @OneToMany(mappedBy = "user")
+    private List<UserGroupEntity> userGroups;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,13 +3,18 @@ DROP TABLE if exists GROUPS;
 CREATE TABLE IF NOT EXISTS `USERS` (
                          `ID`	number 	generated always as identity primary key,
                          `NICKNAME`	nvarchar2	NOT NULL,
-                         `TOKEN`	nvarchar2	NOT NULL,
-                         `GROUP_ID` number NULL
+                         `TOKEN`	nvarchar2	NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `GROUPS` (
                           `ID` number generated always as identity primary key ,
                           `NAME`	nvarchar2  NOT NULL unique
+);
+
+CREATE TABLE IF NOT EXISTS `USER_GROUP` (
+                                        `ID` number generated always as identity primary key ,
+                                        `USER_ID`	number  NOT NULL references USERS(ID),
+                                        `GROUP_ID` number NOT NULL references GROUPS(ID)
 );
 
 INSERT INTO USERS (nickname, token) VALUES ( 'user1', 'token1' );
@@ -29,13 +34,24 @@ INSERT INTO GROUPS (name) VALUES ( 'group3' );
 INSERT INTO GROUPS (name) VALUES ( 'group4' );
 INSERT INTO GROUPS (name) VALUES ( 'group5' );
 
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user11', 'token11' , 1);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user12', 'token12' , 2);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user13', 'token13' , 1);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user14', 'token14' , 2);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user15', 'token15' , 1);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user16', 'token16' , 2);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user17', 'token17' , 1);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user18', 'token18' , 2);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user19', 'token19' , 1);
-INSERT INTO USERS (nickname, token, group_id) VALUES ( 'user20', 'token20' , 2);
+INSERT INTO USERS (nickname, token) VALUES ( 'user11', 'token11' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user12', 'token12' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user13', 'token13' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user14', 'token14' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user15', 'token15' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user16', 'token16' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user17', 'token17' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user18', 'token18' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user19', 'token19' );
+INSERT INTO USERS (nickname, token) VALUES ( 'user20', 'token20' );
+
+INSERT INTO USER_GROUP (user_id, group_id) VALUES ( '11', '1' );
+INSERT INTO USER_GROUP (user_ID, group_id) VALUES ( '12', '2' );
+INSERT INTO USER_GROUP (user_id, group_id) VALUES ( '13', '1' );
+INSERT INTO USER_GROUP (user_ID, group_id) VALUES ( '14', '2' );
+INSERT INTO USER_GROUP (user_id, group_id) VALUES ( '15', '1' );
+INSERT INTO USER_GROUP (user_ID, group_id) VALUES ( '16', '2' );
+INSERT INTO USER_GROUP (user_id, group_id) VALUES ( '17', '1' );
+INSERT INTO USER_GROUP (user_ID, group_id) VALUES ( '18', '2' );
+INSERT INTO USER_GROUP (user_id, group_id) VALUES ( '19', '1' );
+INSERT INTO USER_GROUP (user_ID, group_id) VALUES ( '20', '2' );
