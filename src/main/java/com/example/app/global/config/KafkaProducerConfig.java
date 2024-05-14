57a5e1ed044390bb4cf376fdd5a74798b2aa25b3
ujin_jeong <ujin_jeong@tmax.co.kr>
2024-05-14 10:44:06 +0900

    [del] 경로 변경 : KafkaProducerConfig

--- src/main/java/com/example/app/global/config/KafkaProducerConfig.java
+++ src/main/java/com/example/app/global/config/KafkaProducerConfig.java
@@ -1,9 +1,10 @@
 package com.example.app.global.config;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
